fb112af2bec980e35c4f06d4318037a4bb62ea31
Marcel Voigt <mv@noch.so>
2016-05-21 15:25:27 +0200

    Add package css to extract colors

---
+++ css/css.go
@@ -0,0 +1,208 @@
+package css
+
+import (
+	"errors"
+	"regexp"
+	"strconv"
+	"strings"
+
+	"github.com/lucasb-eyer/go-colorful"
+	"github.com/tdewolff/parse/css"
+	"golang.org/x/net/html"
+)
+
+type ColorMention struct {
+	Color *colorful.Color
+	// color, background-color
+	Property string
+	// .class, nav > a
+	// Selectors for inline CSS are based on Context structs.
+	// Otherwise the typical CSS selector is used.
+	Selector string
+}
+
+type Context []string
+
+var reHex = regexp.MustCompile(`^#([0-9A-Fa-f]{3}|[0-9A-Fa-f]{6})$`)
+var reRGB = regexp.MustCompile(`(?i)rgb\(\s*([0-9]{1,3})\s*,\s*([0-9]{1,3})\s*,\s*([0-9]{1,3})\s*\)`)
+var reRGBPerc = regexp.MustCompile(`(?i)rgb\(\s*([0-9]{1,3})%\s*,\s*([0-9]{1,3})%\s*,\s*([0-9]{1,3})%\s*\)`)
+
+func (c *Context) Push(ctx string) {
+	*c = append(*c, ctx)
+}
+func (c *Context) Pop() (string, error) {
+	l := len(*c)
+	if l == 0 {
+		return "", errors.New("Stack is empty")
+	}
+	ctx := (*c)[l-1]
+	*c = (*c)[:l-1]
+	return ctx, nil
+}
+func (c Context) String() string {
+	return strings.Join(c, " > ")
+}
+
+func New(c *colorful.Color, property, selector string) *ColorMention {
+	return &ColorMention{
+		Color:    c,
+		Property: property,
+		Selector: selector,
+	}
+}
+
+// Extract colors from "style" attributes and elements.
+func ParseHTML(s string) ([]*ColorMention, error) {
+	r := strings.NewReader(s)
+	doc, err := html.Parse(r)
+	if err != nil {
+		return nil, err
+	}
+	var f func(*html.Node)
+	mentions := []*ColorMention{}
+	context := Context{}
+	f = func(n *html.Node) {
+		if n.Type == html.ElementNode {
+			ctx := n.Data
+			// Update context using attributes "id" and "class"
+			for _, attr := range n.Attr {
+				if attr.Key == "id" {
+					ctx += "#" + attr.Val
+				} else if attr.Key == "class" {
+					ctx += "." + attr.Val
+				}
+			}
+			context.Push(ctx)
+
+			// Look for a style="" attribute
+			for _, attr := range n.Attr {
+				if attr.Key == "style" {
+					mentions = append(mentions, parseStyleAttribute(attr.Val, context.String())...)
+				}
+			}
+			// Look for a <style> element
+			if n.Data == "style" {
+				mentions = append(mentions, ParseStylesheet(n.FirstChild.Data)...)
+			}
+		}
+		for c := n.FirstChild; c != nil; c = c.NextSibling {
+			f(c)
+		}
+		if n.Type == html.ElementNode {
+			_, _ = context.Pop()
+		}
+	}
+	f(doc)
+	return mentions, nil
+}
+
+// Extract colors from a full CSS stylesheet.
+func ParseStylesheet(sheet string) []*ColorMention {
+	p := css.NewParser(strings.NewReader(sheet), false)
+	var selector string
+	var cms []*ColorMention
+	for {
+		gt, _, data := p.Next()
+		if gt == css.ErrorGrammar {
+			break
+		}
+		// Remember the selector for the upcoming declarations
+		if gt == css.BeginRulesetGrammar {
+			selector = tokenString(p.Values())
+		}
+		if gt == css.DeclarationGrammar {
+			c, ok := parseColor(p.Values())
+			if ok {
+				cms = append(cms, New(c, string(data), selector))
+			}
+		}
+	}
+	return cms
+}
+
+// tokenString returns a single string from a list of css.Token
+func tokenString(t []css.Token) string {
+	var s string
+	for _, tt := range t {
+		s += string(tt.Data)
+	}
+	return s
+}
+
+// Extracts ColorMentions from an inline CSS (i.e. a style attribute).
+func parseStyleAttribute(s string, selector string) []*ColorMention {
+	p := css.NewParser(strings.NewReader(s), true)
+	var cms []*ColorMention
+	for {
+		gt, _, data := p.Next()
+		if gt == css.ErrorGrammar {
+			break
+		}
+		if gt != css.DeclarationGrammar {
+			continue
+		}
+		c, ok := parseColor(p.Values())
+		if ok {
+			cms = append(cms, New(c, string(data), selector))
+		}
+	}
+	return cms
+}
+
+// Attempts to extract a color from list of CSS tokens.
+// It is expected that the Tokens come from a Declaration.
+func parseColor(t []css.Token) (c *colorful.Color, ok bool) {
+	s := tokenString(t)
+
+	// "#ccc" hex to Color
+	hex := reHex.FindString(s)
+	if hex != "" {
+		c, err := colorful.Hex(hex)
+		if err == nil {
+			return &c, true
+		}
+	}
+
+	// "rgb(0,0,0)" to Color
+	sm := reRGB.FindStringSubmatch(s)
+	if sm != nil {
+		var err error
+		rgb := make([]float64, 3)
+		for i, v := range sm[1:] {
+			rgb[i], err = strconv.ParseFloat(v, 64)
+			if err != nil {
+				return nil, false
+			}
+		}
+		c := colorful.Color{rgb[0] / 255.0, rgb[1] / 255.0, rgb[2] / 255.0}
+		return &c, true
+	}
+
+	// "rgb(0%,0%,0%)" to Color
+	sm = reRGBPerc.FindStringSubmatch(s)
+	if sm != nil {
+		var err error
+		rgb := make([]float64, 3)
+		for i, v := range sm[1:] {
+			rgb[i], err = strconv.ParseFloat(v, 64)
+			if err != nil {
+				return nil, false
+			}
+		}
+		c := colorful.Color{rgb[0] / 100.0, rgb[1] / 100.0, rgb[2] / 100.0}
+		return &c, true
+	}
+
+	// "cyan" named to Color by checking each token individually
+	for _, tt := range t {
+		named, ok := names[string(tt.Data)]
+		if ok {
+			c, err := colorful.Hex(named)
+			if err != nil {
+				continue
+			}
+			return &c, true
+		}
+	}
+	return nil, false
+}
---
+++ css/css_test.go
@@ -0,0 +1,73 @@
+package css
+
+import (
+	"log"
+	"testing"
+
+	"github.com/lucasb-eyer/go-colorful"
+)
+
+func init() {
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+}
+
+var testsHtml = []struct {
+	in  string
+	out []*ColorMention
+}{
+	{
+		`<style>body{color:#001122}</style>`,
+		[]*ColorMention{
+			New(mustHex("#001122"), "color", "body"),
+		},
+	},
+	{
+		`<div style="background-color:#001122"></div>`,
+		[]*ColorMention{
+			New(mustHex("#001122"), "background-color", "html > body > div"),
+		},
+	},
+	{
+		`<style>
+			.rgb { color:rgb(0,1,255) }
+			.rgb-percent { color:rgb(0%,50%,100%) }
+			.named { color:darkgray; }
+		</style>`,
+		[]*ColorMention{
+			New(mustHex("#0001ff"), "color", ".rgb"),
+			New(mustHex("#0080ff"), "color", ".rgb-percent"),
+			New(mustHex("#a9a9a9"), "color", ".named"),
+		},
+	},
+}
+
+func mustHex(h string) *colorful.Color {
+	c, err := colorful.Hex(h)
+	if err != nil {
+		panic(err)
+	}
+	return &c
+}
+
+func TestParseHtml(t *testing.T) {
+	for ti, tt := range testsHtml {
+		cms, err := ParseHTML(tt.in)
+		if err != nil {
+			t.Error(err)
+		}
+		if len(cms) != len(tt.out) {
+			t.Errorf("Test #%d expected %d ColorMentions, got %d", ti, len(tt.out), len(cms))
+		}
+		for ci, cm := range cms {
+			if cm.Color.Hex() != tt.out[ci].Color.Hex() {
+				t.Errorf("Test #%d expected hex %s for ColorMention #%d, got %s", ti, tt.out[ci].Color.Hex(), ci, cm.Color.Hex())
+			}
+			if cm.Property != tt.out[ci].Property {
+				t.Errorf("Test #%d expected property %s for ColorMention #%d, got %s", ti, tt.out[ci].Property, ci, cm.Property)
+			}
+			if cm.Selector != tt.out[ci].Selector {
+				t.Errorf("Test #%d expected selector %s for ColorMention #%d, got %s", ti, tt.out[ci].Selector, ci, cm.Selector)
+			}
+		}
+	}
+}
