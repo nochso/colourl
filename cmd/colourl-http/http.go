be6f24f849bbed2a625883aeeeb125c98482a7cb
Marcel Voigt <mv@noch.so>
2016-05-26 21:07:38 +0200

    Add colourl-http server

---
+++ cmd/colourl-http/http.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	chttpd "github.com/nochso/colourl/http"
+	"log"
+	"net/http"
+)
+
+var port int
+
+func init() {
+	flag.IntVar(&port, "port", 9191, "HTTP listening port")
+	flag.IntVar(&port, "p", 9191, "HTTP listening port (shorthand)")
+}
+
+func main() {
+	flag.Parse()
+	log.Printf("colourl-http port=%d", port)
+	http.HandleFunc("/svg", chttpd.SVGHandler)
+	http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+}
