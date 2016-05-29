fe8eafa675a467cd0d03424d38694411419e05d1
Marcel Voigt <mv@noch.so>
2016-05-29 18:03:01 +0200

    Debug log HTTP requests

--- cmd/colourl-http/http.go
+++ cmd/colourl-http/http.go
@@ -5,7 +5,9 @@ import (
 	"fmt"
 	log "github.com/Sirupsen/logrus"
 	chttpd "github.com/nochso/colourl/http"
+	prefixed "github.com/x-cray/logrus-prefixed-formatter"
 	"net/http"
+	"time"
 )
 
 var (
@@ -25,6 +27,9 @@ func init() {
 
 func main() {
 	flag.Parse()
+	formatter := new(prefixed.TextFormatter)
+	formatter.TimestampFormat = "2006-01-02 15:04:05 Z07:00"
+	log.SetFormatter(formatter)
 	if verbose {
 		log.SetLevel(log.DebugLevel)
 	}
@@ -36,7 +41,20 @@ func main() {
 		"port":    port,
 		"verbose": verbose,
 	}).Info("Starting HTTP server")
-	http.HandleFunc("/", chttpd.IndexHandler)
-	http.HandleFunc("/svg", chttpd.SVGHandler)
+	http.Handle("/", logHandler(chttpd.IndexMux().ServeHTTP))
+	http.HandleFunc("/svg", logHandler(chttpd.SVGHandler))
 	http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+}
+
+func logHandler(fn http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		fn(w, r)
+		log.WithFields(log.Fields{
+			"duration": time.Now().Sub(start),
+			"url":      r.URL,
+			"method":   r.Method,
+			"remote":   r.RemoteAddr,
+		}).Debug("HTTP request")
+	}
 }
